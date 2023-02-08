7441639b29baaa59776cd862c68416b8d69b5147
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-02-08 10:34:14 +0800

    Add files via upload

---
+++ appendListvsTuples.py
@@ -0,0 +1,10 @@
+listy=['Joseph','John']
+listy.append('Johanna')
+print(listy)
+
+tuppy=('Joseph','John')
+print(tuppy)
+tuppy=('Joseph','John','Johanna')
+print(tuppy)
+
+""" tuppy.append('Johanna') will not be worked the append is not available to use in tuples only use in list"""
